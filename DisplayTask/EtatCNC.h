8726b3ed7954ae5d1e93289867592d8f7912d83e
Jean-Pierre de SOZA <ds.jaypee@gmail.com>
2020-10-17 12:11:22 +0200

    Nouvelle version corrigeant le clignotement de l'affichage

--- DisplayTask/EtatCNC.h
+++ DisplayTask/EtatCNC.h
@@ -1,23 +1,31 @@
 #define __ETAT_CNC__
-
-struct EtatCNC 
+#ifndef __WIDGET__
+#include "Widget.h"
+#endif
+#ifndef __COULEURS__
+#include "Couleurs.h"
+#endif
+// Les widget contiennet une copie de la valeurs des paramètres d'état
+// C'est seulement quand on valide l'affichage (bouton Start)
+// Que les données fraîchement éditées sont recopiées dans l'état.
+// Dans la programamtion classique de GUI, c'est le rôle du contrôleur
+// de faire le lien entre l'état du "modèle" et la vue affichée.
+#define ModelViewLinkID int
+// Le trio est abrégé par les 3 lettres MVC Model-View-Controller
+struct ModelViewLink
 {
-  int xCoordMM;
-  int zCoordMM;
-  int outil;
-  int xAvanceMMpm;
-  int zAvanceMMpm;
-  int brocheRPM;
-  bool xSelected;
-  bool xZero;
-  bool zSelected;
-  bool zZero;
-  bool aSelected;
-  bool aZero;
-  bool autoSelected;
-  bool mrSelected;
-  bool mlSelected;
-  bool started;
-  int sauvegarde;
-  int valeurLue; // peut-être abandonnée
+  ModelViewLinkID mvlID;
+  char *name;
+  int modelParam;
+  WidgetID viewWidget;
+};
+
+struct EtatCNC
+{
+  RGB565 fond;
+  WidgetID wWidgetActifndx;
+  int valeurLue;
+  int nbClicks;
+  int nbWidgets;
+  Widget *ecran;
 };
